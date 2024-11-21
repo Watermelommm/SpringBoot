a5a403815ea739404484077da67bc51632ab3df9
Watermelommm <watermelommm@gmail.com>
2024-11-21 14:41:26 +0800

    the second commit

---
+++ src/main/java/com/watermelon/springbootweb/controller/testUpdataController.java
@@ -0,0 +1,11 @@
+package com.watermelon.springbootweb.controller;
+
+import org.apache.catalina.users.SparseUserDatabase;
+
+public class testUpdataController {
+
+    public void printHelloWorld(){
+
+        System.out.println("hello world");
+    }
+}
